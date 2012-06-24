f87d0b6fec55c190bb69136e94c1433afa4f7a00
Farrin Reid <blakmatrix@gmail.com>
2012-06-23 19:37:43 -0800

    [fix] Changed require('util') to require('util') for compatibility with node v0.8

--- tests/readme.js
+++ tests/readme.js
@@ -1,9 +1,9 @@
 var spruce = require('../lib/spruce').init();
-var sys = require('sys');
+var util = require('util');
 spruce.info('Yes! we have no bonanaza!');
 
     var sendMail = function (msg){
-        console.log('Sending '+sys.inspect(msg,true));
+        console.log('Sending '+util.inspect(msg,true));
     };
     var makeMessageBody = function(message){
         return 'what ' + message;
